aebeabe6d0306ec083b126509ecdad3292001888
Michał <66076633+Mishu5@users.noreply.github.com>
2024-09-02 15:04:03 +0200

    added modules for postgresql

--- volleyball-scoreboard/routes/index.js
+++ volleyball-scoreboard/routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('./db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  try {
+    const result = await pool.query('SELECT * FROM uzytkownicy');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+
   res.render('index', { title: 'Express' });
 });
 
